63ed201edd602d7bad8d742856b892e1dcee7eda
Jon Couldridge <jonathan.couldridge@nottingham.ac.uk>
2020-02-27 15:44:11 +0000

    stories use CSF

--- src/Component.stories.js
+++ src/Component.stories.js
@@ -1,14 +1,18 @@
 import React from "react";
-import { storiesOf } from "@storybook/react";
 import { text } from "@storybook/addon-knobs";
 import { action } from "@storybook/addon-actions";
 import Component from "./Component";
+
+export default {
+  title: "Component",
+  component: Component
+};
 
 const actions = {
   logResults: action("Results logged"),
   setNextEnabled: action("Next button enabled")
 };
 
-storiesOf("Component", module).add("Default", () => (
+export const Basic = () => (
   <Component text={text("Text", "Hello")} {...actions} />
-));
+);
